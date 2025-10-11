00b5717b6b802354e04a382d6a5ac8884b2bfda1
donz <donnygapulao@gmail.com>
2025-10-11 20:17:37 +0800

    added return data

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,9 @@ import type { Ref } from "vue";
 export async function fetchData(url: string, state: Ref<any>) {
   try {
     const response = await fetch(url);
-    state.value = await response.json();
+    const data = await response.json();
+    state.value = data;
+    return data;
   } catch (error) {
     console.error(error);
   }
